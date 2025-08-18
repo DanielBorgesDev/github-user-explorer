cb2977ac64c7ac499d82c758f97052c6af62e28f
Daniel <danielnorges@gmail.com>
2025-08-18 18:03:21 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export function App() {
+  return (
+    <div style={{ padding: 24, fontFamily: 'system-ui, Arial' }}>
+      <h1>✅ Vite + React + TS funcionando</h1>
+      <p>Se você vê isso, o pipeline básico de render está OK.</p>
+    </div>
+  )
+}
